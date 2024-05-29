dd10fd3e16ab88d97d1d53e2b2bed2e231d8cddf
Maria Eduarda <168425368+medusn@users.noreply.github.com>
2024-05-29 10:17:27 -0300

    Update QuestaoOnze.java

--- QuestaoOnze.java
+++ QuestaoOnze.java
@@ -11,7 +11,7 @@ public class QuestaOnze {
 		int a = 0, b = 1;
 		
 		for (int i = 0; a <= num; i++) {
-			System.out.println(a + " ");
+			System.out.println(a);
 			int valorAtual = a;
 			a = b;
 			b = valorAtual + b;
