019ac1769c1856c94272fc38843cecd45161df04
Maria Eduarda <168425368+medusn@users.noreply.github.com>
2024-05-29 10:04:56 -0300

    Create QuestaoOnze.java

---
+++ QuestaoOnze.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class QuestaOnze {
+	public static void main(String[] args) {
+		Scanner ref = new Scanner(System.in);
+	
+	System.out.println("Digite um número: ");
+		int num = ref.nextInt();
+		
+	System.out.println("Sequência de Fibonacci do número " + num + ": ");
+		int a = 0, b = 1;
+		
+		for (int i = 0; a <= num; i++) {
+			System.out.println(a + " ");
+			int valorAtual = a;
+			a = b;
+			b = valorAtual + b;
+		}
+	}
+}
